956cd6709be1a71d61470fbeebffb805ae18a4a9
jsvo2 <jsvo2@pdx.edu>
2022-07-20 20:57:18 -0700

    pushing changes before break

--- RPNCalculatorRoom1/src/it/java/edu/pdx/cs410J/Room1/KataIT.java
+++ RPNCalculatorRoom1/src/it/java/edu/pdx/cs410J/Room1/KataIT.java
@@ -9,9 +9,17 @@ import static org.hamcrest.core.StringContains.containsString;
 class KataIT extends InvokeMainTestCase {
 
   @Test
-  void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
-    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class);
-    assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
+  void div() {
+    String [] args = {"20", "5", "/"};
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, args);
+    assertThat(result.getTextWrittenToStandardError(), containsString(""));
+  }
+
+  @Test
+  void add(){
+    String [] args = {"4","2","+","3","-"};
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, args);
+    assertThat(result.getTextWrittenToStandardError(), containsString(""));
   }
 
 
--- RPNCalculatorRoom1/src/main/java/edu/pdx/cs410J/Room1/Kata.java
+++ RPNCalculatorRoom1/src/main/java/edu/pdx/cs410J/Room1/Kata.java
@@ -1,5 +1,8 @@
 package edu.pdx.cs410J.Room1;
 
+import java.lang.*;
+import java.util.*;
+
 /**
  * A class for getting started with a code kata
  *
@@ -7,8 +10,125 @@ package edu.pdx.cs410J.Room1;
  * class (and its tests).
  */
 public class Kata {
+  /*class Node{
+    int a;
+    int b;
+    Node root;
+    Node left;
+    Node right;
+    int value;
+    public Node getRight(){
+      return this.right;
+    }
+    public Node getLeft(){
+      return this.left;
+    }
+    public Node insertLeft(Node left){
+      this.left = left;
+      return left;
+    }
+    public Node insertRight(Node right){
+      this.right = right;
+      return right;
+    }
+    Kata(int a, int b) {
+      this.a = a;
+      this.b = b;
+    }
+  }
+  class Sub extends Node {
+    int exec() {
+      return this.a - this.b;
+    }
+  }
+  class Add extends Node {
+    int exec() {
+      return this.a + this.b;
+    }
+  }
+  class Mul extends Node {
+    int exec(){
+      return this.a * this.b;
+    }
+  }
+  class Div extends Node {
+    int exec() {
+      return Math.floor(this.a / this.b);
+    }
+  }
+  class Sqrt extends Node {
 
+  }*/
+    /**
+     *        // int a;
+     *        // int b;
+     *        // int total = 0;
+     * 3 5 8 * 7 + *
+     *
+     *
+     * 3 5 8   *
+     * stop
+     *    // op = *
+     *    // a = 8
+     *    // b = 5
+     *    // total += 5 * 8
+     *    // push total back to stack
+     * 3 40
+     * 3 40 7
+     * 3  40 7 +
+     * stop
+     *    // op = +
+     *    // a = 7
+     *    // b = 40
+     *    // total += 7 + 40
+     *    // push total back to stack
+     * 3 47
+     * *
+     *  // op = *
+     *  // a = 47
+     *  // b = 3
+     *  // total += 47 *3
+     * no more on stack
+     * end
+     *
+     * */
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+    Stack stack = new Stack<Integer>();
+    for (String value : args){
+      if (value.equals("*")){
+        // pop pop mult push
+        Integer a = (Integer)stack.pop();
+        Integer b = (Integer)stack.pop();
+        Integer res = a * b;
+        stack.push(res);
+      } else if (value.equals("/")){
+        // pop pop div push
+        Integer a = (Integer)stack.pop();
+        Integer b = (Integer)stack.pop();
+        Integer res = a / b;
+        stack.push(res);
+      } else if (value.equals("+")){
+        // pop pop plus push
+        Integer a = (Integer)stack.pop();
+        Integer b = (Integer)stack.pop();
+        Integer res = a + b;
+        stack.push(res);
+      } else if (value.equals("-")){
+        // pop pop minux push
+        Integer a = (Integer)stack.pop();
+        Integer b = (Integer)stack.pop();
+        Integer res = a - b;
+        stack.push(res);
+      }else if("MAX"){
+        Integer a = (Integer) stack.pop();
+        Integer b = (Integer) stack.pop();
+        Integer res = (a > b) ? a : b;
+        stack.push(res);
+      } else{
+        stack.push(Integer.valueOf(value));
+      }
+    }
+    // an empty stack
+    System.out.println(stack.pop());
   }
 }
